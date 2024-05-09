d1710fee34fadc2613dd05d0ce661a3961ef8ebb
Chrome Extensions <123759385+Hexcein-moonsters@users.noreply.github.com>
2024-05-09 17:28:32 +0200

    Oops

--- dist/simulation_worker.bundle.js
+++ dist/simulation_worker.bundle.js
@@ -18392,7 +18392,7 @@ let simulationSpeed = 0.002
                 .length; ++e)
                 hp(this, sp, "f")[e](1 / hp(this, rp, "f"));
             hp(this, qf, "f")
-                .stepSimulation(simulationSpeed, 0, 1 / hp(this, rp, "f")), cp(this, np, (e = hp(this, np, "f"), ++e), "f");
+                .stepSimulation(1/hp(this, rp, "f"), 0, 1 / hp(this, rp, "f")), cp(this, np, (e = hp(this, np, "f"), ++e), "f");
             for (let e = 0; e < hp(this, ap, "f")
                 .length; ++e)
                 hp(this, ap, "f")[e](1 / hp(this, rp, "f"))
